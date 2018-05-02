0b46400dbbddabc9e5c552f4dcb2c7e8808fed64
Lughween <36704115+Lughween@users.noreply.github.com>
2018-05-02 17:43:03 +0200

    Update Squelette_test1.ino

--- Squelette_test1.ino
+++ Squelette_test1.ino
@@ -10,8 +10,11 @@
 // mp3 includes
 #include <SPI.h>
 #include <SdFat.h>
-#include <FreeStack.h> 
+#include <FreeStack.h>
 #include <SFEMP3Shield.h>
+
+SdFat sd;
+SdFile file;
 
 // mp3 variables
 SFEMP3Shield MP3player;
@@ -19,11 +22,11 @@ byte result;
 int lastPlayed = 0;
 
 // mp3 behaviour defines
-#define REPLAY_MODE TRUE  // By default, touching an electrode repeatedly will 
+#define REPLAY_MODE TRUE  // By default, touching an electrode repeatedly will
                           // play the track again from the start each time.
                           //
-                          // If you set this to FALSE, repeatedly touching an 
-                          // electrode will stop the track if it is already 
+                          // If you set this to FALSE, repeatedly touching an
+                          // electrode will stop the track if it is already
                           // playing, or play it from the start if it is not.
 
 // touch behaviour definitions
@@ -35,24 +38,32 @@ int lastPlayed = 0;
 /*-------------------------------------------------------------------------------------------------------------------
                                                 Definir les pin pour chaque zone de contact
  ---------------------------------------------------------------------------------------------------------------------*/
-#define rArm 0
-#define lArm 1
-#define rLeg 2
-#define lLeg 3
+#define rHand 0
+#define lHand 1
+#define rArm 2
+#define lArm 3
+#define rLeg 4
+#define lLeg 5
 
+
+#define pinMic A0
 /*-------------------------------------------------------------------------------------------------------------------
                                                 Initialisation des différents mode
  ---------------------------------------------------------------------------------------------------------------------*/
 #define pinSwitchMode 13
-bool isGameMode = false;
 
- 
-void setup() {
-    Serial.begin(57600);
-  
+
+void setup()
+{
+
+  /*------------------------------------------------------------------------------
+                               Setup Touch
+  ------------------------------------------------------------------------------*/
+  Serial.begin(57600);
+
   pinMode(LED_BUILTIN, OUTPUT);
-   
-  //while (!Serial) ; {} //uncomment when using the serial monitor 
+
+  //while (!Serial) ; {} //uncomment when using the serial monitor
   Serial.println("Bare Conductive Touch MP3 player");
 
   if(!sd.begin(SD_SEL, SPI_HALF_SPEED)) sd.initErrorHalt();
@@ -68,56 +79,90 @@ void setup() {
                                     // higher values are less sensitive, lower values are more sensitive
                                     // for proximity operation, you could try a value of 3
                                     // this must ALWAYS be lower than the touch threshold
-  
+
 
   result = MP3player.begin();
   MP3player.setVolume(10,10);
- 
+
   if(result != 0) {
     Serial.print("Error code: ");
     Serial.print(result);
     Serial.println(" when trying to start MP3 player");
    }
+/*------------------------------------------------------------------------------
+                                Setup Micro
+ ------------------------------------------------------------------------------*/
+ pinMode(pinMic, INPUT);
 
+/*------------------------------------------------------------------------------
+                             Jauge d'anxiété
+------------------------------------------------------------------------------*/
+int Anxiety = 0;
 }
 
-void isRightArmTouched(){
-  if MPR121.isNewTouch(rArm) return true;
+/*------------------------------------------------------------------------------
+                             Détection des touche
+------------------------------------------------------------------------------*/
+bool isRightArmTouched(){
+  if (MPR121.isNewTouch(rArm)) return true;
   return false;
 }
 
-void isLeftArmTouched(){
-  if MPR121.isNewTouch(lArm) return true;
+bool isLeftArmTouched(){
+  if (MPR121.isNewTouch(lArm)) return true;
   return false;
 }
 
-void isRightLegTouched(){
-  if MPR121.isNewTouch(rLeg) return true;
+bool isRightLegTouched(){
+  if (MPR121.isNewTouch(rLeg)) return true;
   return false;
 }
 
-void isLeftLegTouched(){
-  if MPR121.isNewTouch(lLeg) return true;
+bool isLeftLegTouched(){
+  if (MPR121.isNewTouch(lLeg)) return true;
   return false;
 }
 
-void loop() {
- if(isRightArmTouched())
- {
-  //mettre ici comment doit réagir le squelette
- }
- if(isLeftArmTouched())
- {
-  //mettre ici comment doit réagir le squelette
- }
- if(isRightLegTouched())
- {
-  //mettre ici comment doit réagir le squelette
- }
- if(isLeftLegTouched())
- {
-  //mettre ici comment doit réagir le squelette
- }
-  
+bool isLeftHandTouched()
+{
+  if (MPR121.isNewTouch(lHand)) return true;
+  return false;
+}
+
+bool isRightHandTouched()
+{
+  if(MPR121.isNewTouch(rHand)) return true;
+  return false;
+}
+/*------------------------------------------------------------------------------
+                             Gestion des Etats d'anxieté
+------------------------------------------------------------------------------*/
+void setStateToAnxious()
+{
 
 }
+
+void setStateToVeryAnxious()
+{
+
+}
+
+void setStateToQuiet()
+{
+
+}
+
+void setStateToUnQuiet()
+{
+
+}
+void loop()
+{
+  delay(500);
+  int volume = analogRead(A0);
+  Serial.print(volume);
+  Serial.print('\n');
+
+}
+
+
